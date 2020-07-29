40cbf9e15e940d7100abca4f29b35566283c4775
sadman hossain <61970550+sadman123456@users.noreply.github.com>
2020-07-29 18:26:37 +0600

    xml lab task

---
+++ lab-5[PHP]/public.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Public Home</title>
+</head>
+
+<body>
+    <table width="1000px" border="1" cellpadding="0" cellspacing="0" align="center">
+        <tr height = "50px">
+            <td  align="Left">
+            <h1>Xcompany</h1>
+          
+            <Td colspan="2" align="right"><a href="#">Home</a>
+                <a href="registration.php">Registration</a>
+                <a href="login.php">Login</a>
+            </td>
+        </tr>
+        <tr height = "150px">
+        	<td colspan="2">Welcome to xCompany</td>
+        </tr>
+        <tr height = "40px">
+        	<td colspan="2" align="center">Copyright@ 2017</td>
+        </tr>
+    </table>
+</body>
+
+</html>
